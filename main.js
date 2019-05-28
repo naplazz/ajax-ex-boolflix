a02838dc2c8e2cc793603c7bcacb27379e8a290b
naplazz <naplazz@gmail.com>
2019-05-28 15:57:52 +0200

    fine

--- main.js
+++ main.js
@@ -38,6 +38,7 @@ $(document).ready(function() {
             foto: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + data.results[i].backdrop_path
           };
           var html = template(context);
+
           $('.display-result').append(html)
           // fine hendlebars template
 
@@ -49,7 +50,69 @@ $(document).ready(function() {
         alert('errore')
       }
 
-    });
+   });   //fine ajax film
+
+   $.ajax({
+     url: 'https://api.themoviedb.org/3/search/tv',
+     type: 'GET',
+     data: {
+       api_key: '32d3f80d6efc4febfeada2bd492caf35',
+       query: $('#searchBox').val(),
+       language: 'it',
+
+     },
+     success: function(data) {
+       $('.display-serie').empty()
+      
+       for (var i = 0; i < data.results.length; i++) {
+         // hendlebars template
+         var icon = '<i class="fas fa-star"></i>';
+         var iconhtml = '';
+         var numeroStelle = parseInt(data.results[i].vote_average / 2);
+         if (data.results[i].original_language == "en") {
+           var lingua = 'gb';
+         } else {
+           var lingua = data.results[i].original_language;
+         }
+         for (var z = 0; z <= numeroStelle -1; z++) {
+           iconhtml = iconhtml  + icon;
+         }
+         var sourceSerie = document.getElementById("serieTemplate").innerHTML;
+         var templateSerie = Handlebars.compile(sourceSerie);
+         var contextSerie = {
+           movieOrigTitle: data.results[i].original_name,
+           movieTitle: data.results[i].name,
+           lingua: lingua,
+           voto: data.results[i].vote_average,
+           stelle: iconhtml,
+           foto: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + data.results[i].backdrop_path
+         };
+         var htmlSerie = templateSerie(contextSerie);
+
+         $('.display-serie').append(htmlSerie)
+         // fine hendlebars template
+
+       }
+       $('#searchBox').val("");
+       $('.movie-card').hover(function() {
+         /* Stuff to do when the mouse enters the element */
+         $(this).find('img').hide()
+         $(this).find('.card-details').toggleClass('hidden');
+       }, function() {
+         $(this).find('img').show()
+         $(this).find('.card-details').toggleClass('hidden');
+
+       });
+
+     },
+     error: function() {
+       alert('errore')
+     }
+
+  });
+
+
+
 
   });
 
