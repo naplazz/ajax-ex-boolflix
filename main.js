c116a919502540693ab2096a127e2a575bc548e7
naplazz <31731772+naplazz@users.noreply.github.com>
2019-05-28 10:58:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,9 +9,11 @@ $(document).ready(function() {
       data: {
         api_key: '32d3f80d6efc4febfeada2bd492caf35',
         query: $('#searchBox').val(),
+        language: 'it',
 
       },
       success: function(data) {
+        $('.display-result').empty()
         for (var i = 0; i < data.results.length; i++) {
           // hendlebars template
           var icon = '<i class="fas fa-star"></i>'
@@ -23,13 +25,16 @@ $(document).ready(function() {
             movieTitle: data.results[i].title,
             lingua: data.results[i].original_language,
             voto: data.results[i].vote_average,
-            stelle: numeroStelle
+            stelle: numeroStelle,
+            foto: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + data.results[i].backdrop_path
           };
           var html = template(context);
           $('.display-result').append(html)
-          // fine hendlebars templat
+          // fine hendlebars template
 
         }
+        $('#searchBox').val("")
+
       },
       error: function() {
         alert('errore')
